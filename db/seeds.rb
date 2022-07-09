dbba6b330c502cf195e2b99a2a05ef502e300d35
january amanda january.amanda@gmail.com <january.amanda@gmail.com>
2022-07-09 13:12:11 +0900

    Watch List Models done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# puts "Destroy all the movies, lists, bookmarks"
+# Movie.destroy_all
+# List.destroy_all
+# Bookmark.destroy_all
+
+# puts "Creating movie"
+# 48.times do
+#   Movie.create!(
+#     title: Faker::Movie.title,
+#     overview: Faker::Movie.quote,
+#   )
+# end
